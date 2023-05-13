9be9f82eceea6119a954b5e9fe7b54c8b0e3bc09
Eugene <e@egeek.me>
2023-05-13 19:04:07 +0300

    Use `hspec-discover` to find all specs automatically

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,15 +1,1 @@
-module Main (main) where
-
-import Adapters.ServiceSpec qualified (spec)
-import Domain.LiveStatusSpec qualified (spec)
-import Domain.YoutubeFeedSpec qualified (spec)
-import Test.Hspec
-import Usecases.AudioFeedSpec qualified (spec)
-
--- FIXME use hspec-discover
-main :: IO ()
-main = hspec $ do
-  Domain.LiveStatusSpec.spec
-  Domain.YoutubeFeedSpec.spec
-  Usecases.AudioFeedSpec.spec
-  Adapters.ServiceSpec.spec
+{-# OPTIONS_GHC -Wno-missing-export-lists -F -pgmF hspec-discover #-}
