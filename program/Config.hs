142daf39f004a74f5bbedb98b4343b4ad2b3b1be
eugene <e@egeek.me>
2024-06-14 20:32:28 +0300

    Allow to set the server hostname for the feeds

--- program/Config.hs
+++ program/Config.hs
@@ -2,11 +2,13 @@ module Config
   ( Config (..)
   ) where
 
+import Data.ByteString (ByteString)
 import Network.Wai.Handler.Warp (Port)
 import Options.Generic (Generic, ParseRecord, Wrapped, (:::), type (<!>), type (<#>), type (<?>))
 
-newtype Config w = Config
+data Config w = Config
   { port :: w ::: Port <#> "p" <!> "8080" <?> "Port to listen on"
+  , host :: w ::: ByteString <#> "h" <!> "localhost" <?> "Server's hostname to use in the feeds"
   }
   deriving stock (Generic)
 
